fd7c567e1ad660d7e77f701996836b4fc8214a14
sivanargana <siva.nargana66@gmail.com>
2024-03-29 20:31:41 +0530

    comit

---
+++ wp-content/themes/theme@2024/header.php
@@ -0,0 +1,2 @@
+<?php wp_head(); ?>
+<link rel="stylesheet" href="<?php bloginfo("stylesheet_url"); ?>">
---
+++ wp-content/themes/theme@2024/index.php
@@ -0,0 +1,4 @@
+<?php get_header(); ?>
+<?php /* Template Name:Home */ ?>
+<?php the_content(); ?>
+<?php get_footer(); ?>
